8f8b9c1d25a0407648d7b1fa3d283e0c529727c9
Lei Yang <ehp246@yahoo.com>
2024-05-14 16:21:14 -0400

    Using Id to connect

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ import logging
 app = func.FunctionApp()
 
 @app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="m3job-event-local-leiyang",
-                               connection="evhns3fcsl7aplyiuy_SL_EVENTHUB") 
+                               connection="EventHubConnection") 
 def eventhub_trigger(azeventhub: func.EventHubEvent):
     logging.info('Python EventHub trigger processed an event: %s',
                 azeventhub.get_body().decode('utf-8'))
